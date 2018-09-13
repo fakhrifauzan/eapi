28be14457de3f54b58ea5ee3b22072b5211893ff
Fakhri Fauzan <fazan697@gmail.com>
2018-09-13 12:47:41 +0700

    update product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -62,7 +62,9 @@ class ProductController extends Controller
      */
     public function update(Request $request, Product $product)
     {
-        //
+        $request['detail']= $request->description;
+        unset($request['description']);
+        $product->update($request->all());
     }
 
     /**
